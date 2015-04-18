d0f99725cfc80d8052a9add6ac8637735f8eca91
Jan Henning Thorsen <jhthorsen@cpan.org>
2015-04-18 13:14:04 +0200

    Released version 3.3007
     - Improved documentation
     - Compatible with AssetPack 0.44

--- lib/Mojolicious/Plugin/Bootstrap3.pm
+++ lib/Mojolicious/Plugin/Bootstrap3.pm
@@ -6,7 +6,7 @@ Mojolicious::Plugin::Bootstrap3 - Mojolicious + http://getbootstrap.com/
 
 =head1 VERSION
 
-3.3006
+3.3007
 
 =head1 DESCRIPTION
 
@@ -223,7 +223,7 @@ use File::Spec ();
 use Cwd        ();
 use constant DEBUG => $ENV{MOJO_ASSETPACK_DEBUG} || 0;
 
-our $VERSION = '3.3006';
+our $VERSION = '3.3007';
 our $OVERRIDE;    # ugly hack. might go away
 
 $ENV{SASS_PATH} ||= '';
